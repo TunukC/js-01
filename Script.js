43c0d3a541a676c1227525af223432585b4db96f
Ana <83139776+TunukC@users.noreply.github.com>
2021-10-22 21:25:15 +0600

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,14 @@
+//Выведите в консоль ваше имя.//
+
+alert = prompt("your userName?");
+alert = prompt("your LastName?");
+console.log("Your userName " + " Your LastName ");
+
+// выводит сообщение в консоль //
+
+console.log("<h1> welcome to java script !</h1>");
+
+// делает в данном фрагменте знак //
+console .log("200+2021");
+
+document .write( "200+2021 " + (200+2021));
